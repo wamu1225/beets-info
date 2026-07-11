88c63ddf859c288460de86f11227c1412f245f84
masaki <azalea1225@gmail.com>
2026-07-12 08:47:47 +0900

    footer: 編集方針(ポータル editorial-policy)へのリンク追加

--- src/App.tsx
+++ src/App.tsx
@@ -469,6 +469,7 @@ function Footer() {
           href={`${BASE}/privacy/`}
           onClick={(e) => { e.preventDefault(); navigateTo('/privacy/'); }}
         >プライバシーポリシー</a>
+        <a href="https://study-apps.com/editorial-policy/">編集方針</a>
         <a href="https://study-apps.com/">study-apps.com</a>
       </div>
       <div style={{ marginTop: 8 }}>
